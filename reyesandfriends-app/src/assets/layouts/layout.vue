e5fa8378bf9914a6b1c80c459ef9bbf4af6d3894
Marcos Reyes <astronautmarkus@gmail.com>
2025-03-23 03:09:14 -0300

    feat: Add Navbar component and enhance layout structure with styling

--- reyesandfriends-app/src/assets/layouts/layout.vue
+++ reyesandfriends-app/src/assets/layouts/layout.vue
@@ -1,14 +1,37 @@
 <template>
-  <div>
-
-    <slot />
-
+  <div class="layout">
+    <Navbar />
+    <main class="content">
+      <slot />
+    </main>
   </div>
 </template>
 
 <script setup>
-
+import Navbar from "../../components/Navbar/Navbar.vue";
 </script>
 
+<style scoped>
+.layout {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
 
+.content {
+  flex: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 2rem;
+}
 
+nav {
+  width: 100%;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 1000;
+}
+
+</style>
---
+++ reyesandfriends-app/src/components/Navbar/Navbar.vue
@@ -0,0 +1,43 @@
+<template>
+    <nav class="navbar has-background-dark py-2" role="navigation" aria-label="main navigation">
+      <div class="container">
+        <div class="navbar-brand">
+          <a class="navbar-item" href="#">
+            <img src="#" alt="Logo" height="28">
+          </a>
+        </div>
+  
+        <div class="navbar-menu">
+          <div class="navbar-start">
+            <a class="navbar-item" href="#">ReyesAndFriends.cl</a>
+            <a class="navbar-item" href="#">Proyectos</a>
+  
+            <div class="navbar-item has-dropdown is-hoverable">
+              <a class="navbar-link">Servicios</a>
+              <div class="navbar-dropdown">
+                <a class="navbar-item" href="#">Consultoría</a>
+                <a class="navbar-item" href="#">Desarrollo</a>
+                <a class="navbar-item" href="#">Soporte</a>
+              </div>
+            </div>
+  
+            <a class="navbar-item" href="#">Contacto</a>
+          </div>
+        </div>
+      </div>
+    </nav>
+  </template>
+  
+  
+  <style scoped>
+  .navbar-item {
+    font-family: 'IBM Plex Sans', sans-serif;
+    font-size: 1rem;
+    color: #141414;
+  }
+  
+  .navbar-item:hover {
+    color: #000;
+  }
+  </style>
+  
