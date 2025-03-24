33e3802212646628a1679039d71b25ab4549c41d
Marcos Reyes <astronautmarkus@gmail.com>
2025-03-23 23:34:25 -0300

    feat: Revamp About page with team introduction and member details

--- reyesandfriends-app/src/pages/About.vue
+++ reyesandfriends-app/src/pages/About.vue
@@ -1,21 +1,107 @@
 <template>
-    <div class="services-list">
-        <h1>About</h1>
-        <p>Welcome to the services page. Here you can find a list of our services.</p>
+    <div class="about-page section">
+        <div class="container">
+            <h1 class="title has-text-centered mt-4 mb-4">Sobre nosotros: Reyes&Friends</h1>
+            <div class="columns is-multiline">
+
+                <div class="column is-12">
+                    <div class="card">
+                        <div class="card-content">
+                            <p class="title">¿Quienes somos?</p>
+                            <p class="content mt-2">
+                                <strong>Reyes&Friends</strong> es una empresa de tecnología construida por <strong>Marcos Reyes</strong> junto a su familia y amigos.
+                                Nuestro objetivo es crear soluciones tecnológicas innovadoras que ayuden a empresas e interesados.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="column is-12">
+                    <p class="title has-text-centered mt-4">Nuestro Equipo</p>
+                    <p class="subtitle has-text-centered mb-4">Nuestro equipo está compuesto por:</p>
+                    <div class="columns is-multiline is-centered">
+
+                        <div class="column is-2">
+                            <div class="card is-small">
+                                <div class="card-image">
+                                    <figure class="image is-1by1">
+                                    <img>
+                                    </figure>
+                                </div>
+                                <div class="card-content">
+                                    <p class="title is-6 has-text-centered">Marcos Reyes</p>
+                                    <p class="subtitle is-7 has-text-centered">Desarollador principal</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="column is-2">
+                            <div class="card is-small">
+                                <div class="card-image">
+                                    <figure class="image is-1by1">
+                                    <img src="">
+                                    </figure>
+                                </div>
+                                <div class="card-content">
+                                    <p class="title is-6 has-text-centered">Isaura Martínez</p>
+                                    <p class="subtitle is-7 has-text-centered">Gerente</p>
+                                </div>
+                            </div>
+                        </div>
+        
+                    </div>
+                </div>
+
+                <div class="column is-12">
+                    <p class="title has-text-centered mt-4">Compañeros y amigos</p>
+                    <p class="subtitle has-text-centered mb-4">Miembros relacionados a nuestra empresa:</p>
+                    <div class="columns is-multiline is-centered mt-4">
+                        <div class="column is-2">
+                            <div class="card is-small">
+                                <div class="card-image">
+                                    <figure class="image is-1by1">
+                                    <img>
+                                    </figure>
+                                </div>
+                                <div class="card-content">
+                                    <p class="title is-6 has-text-centered">Fernando Catalán</p>
+                                    <p class="subtitle is-7 has-text-centered">Analista Programador</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="column is-2">
+                            <div class="card is-small">
+                                <div class="card-image">
+                                    <figure class="image is-1by1">
+                                    <img>
+                                    </figure>
+                                </div>
+                                <div class="card-content">
+                                    <p class="title is-6 has-text-centered">Kiara Rubio</p>
+                                    <p class="subtitle is-7 has-text-centered">Ingeniera en informática</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="column is-2">
+                            <div class="card is-small">
+                                <div class="card-image">
+                                    <figure class="image is-1by1">
+                                    <img>
+                                    </figure>
+                                </div>
+                                <div class="card-content">
+                                    <p class="title is-6 has-text-centered">Miembro 3</p>
+                                    <p class="subtitle is-7 has-text-centered"></p>
+                                </div>
+                            </div>
+                        </div>
+
+
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
-
-<style scoped>
-.services-list {
-    padding: 20px;
-    font-family: Arial, sans-serif;
-}
-
-h1 {
-    color: #333;
-}
-
-p {
-    color: #666;
-}
-</style>
