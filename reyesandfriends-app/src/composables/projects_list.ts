5bd76d62c9873216d63d30288b1d20e32b9bd961
Marcos Reyes <astronautmarkus@gmail.com>
2025-03-23 22:45:07 -0300

    feat: Add Projects page with project listing and details WIP

---
+++ reyesandfriends-app/src/composables/projects_list.ts
@@ -0,0 +1,22 @@
+export function useProjectsList() {
+    return [
+        {
+            id: 1,
+            name: 'Project Alpha',
+            description: 'A cutting-edge project focused on AI development.',
+            image: '/assets/images/project-alpha.jpg'
+        },
+        {
+            id: 2,
+            name: 'Project Beta',
+            description: 'An innovative platform for e-commerce solutions.',
+            image: '/assets/images/project-beta.jpg'
+        },
+        {
+            id: 3,
+            name: 'Project Gamma',
+            description: 'A sustainable energy initiative for the future.',
+            image: '/assets/images/project-gamma.jpg'
+        }
+    ];
+}
--- reyesandfriends-app/src/pages/Projects.vue
+++ reyesandfriends-app/src/pages/Projects.vue
@@ -1,21 +1,37 @@
 <template>
-    <div class="services-list">
-        <h1>Projects</h1>
-        <p>Welcome to the services page. Here you can find a list of our services.</p>
+    <div class="projects-list container">
+        <h1 class="title is-1 mt-4 has-text-centered">Our Projects</h1>
+        <p class="subtitle is-5 has-text-centered">Here are some of the projects we are currently working on:</p>
+        <div class="columns is-multiline">
+            <div v-for="project in projects" :key="project.id" class="column is-one-third">
+                <div class="card">
+                    <div class="card-image">
+                        <figure class="image is-4by3">
+                            <img :src="project.image" :alt="project.name" />
+                        </figure>
+                    </div>
+                    <div class="card-content">
+                        <p class="title is-4">{{ project.name }}</p>
+                        <p class="content">{{ project.description }}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
+<script>
+import { ref } from 'vue';
+import { useProjectsList } from '../composables/projects_list';
+
+export default {
+    setup() {
+        const projects = ref(useProjectsList());
+        return { projects };
+    }
+};
+</script>
+
 <style scoped>
-.services-list {
-    padding: 20px;
-    font-family: Arial, sans-serif;
-}
-
-h1 {
-    color: #333;
-}
-
-p {
-    color: #666;
-}
+/* No additional styles needed as Bulma is being used */
 </style>
