e9b051112ebfc0f92eb4d579c9aad215cb2fb31b
Marcos Reyes <astronautmarkus@gmail.com>
2025-03-24 00:13:45 -0300

    feat: Add new section for ongoing projects and dedicated products on Projects page

--- reyesandfriends-app/src/pages/Projects.vue
+++ reyesandfriends-app/src/pages/Projects.vue
@@ -3,6 +3,7 @@
         <h1 class="title is-1 mt-4 mb-2 has-text-centered">Nuestros proyectos</h1>
         <p class="subtitle is-5 has-text-centered">En este apartado presentamos algunos de los proyectos que nos encontramos trabajando:</p>
         <div>
+            <p class="title is-3 mt-4 has-text-centered">Proyectos en desarrollo</p>
             <section v-for="project in projects" :key="project.id" class="section">
                 <div class="columns is-vcentered">
                     <div class="column is-half">
@@ -17,6 +18,23 @@
                 </div>
             </section>
         </div>
+
+        <div>
+            <h1 class="title is-4 mt-4 mb-2 has-text-centered">Nuestros productos dedicados</h1>
+            <p class="subtitle is-5 has-text-centered mb-5">
+                En Reyes&Friends a parte de ofrecer software personalizado tenemos
+                productos dedicados a tu empresa/negocio que te ayudarán a mejorar
+                la gestión de tu empresa, automatizar procesos o tareas y a mejorar la productividad.
+            </p>
+
+            <div class="mt-6 mb-6 has-text-centered">
+                <p class="is-italic">
+                    ¡Muy pronto podrás explorar estos proyectos en acción, diseñados especialmente para ti!
+                </p>
+            </div>
+
+        </div>
+
     </div>
 </template>
 
@@ -31,7 +49,3 @@ export default {
     }
 };
 </script>
-
-<style scoped>
-/* No additional styles needed as Bulma is being used */
-</style>
